8dac3ae037fa7c291e72d45ac0eaad9c03a0d618
Penguin <user@user.com>
2022-03-01 12:28:50 +0530

    Completed the calculator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const ops = ["/", "-", "+", "*", "."];
+
+  const updatecalc = (value) => {
+    if (
+      (ops.includes(value) && calc === "") ||
+      (ops.includes(value) && ops.includes(calc.slice(-1)))
+    ) {
+      return;
+    }
+    setCalc(calc + value);
+
+    if (!ops.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+  };
+
+  const declResult = () => {
+    setCalc(eval(calc).toString());
+    setResult('')
+  }
+
+  const deleteCalc = () => {
+    setCalc("");
+    setResult("");
+  };
+
+  const createDigits = () => {
+    const digits = [];
+
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button onClick={() => updatecalc(i.toString())} key={i}>
+          {i}
+        </button>
+      );
+    }
+    return digits;
+  };
   return (
     <div className="App">
-      
+      <div className="calc">
+        <div className="display">
+          {result ? <span>({result})</span> : ""}
+          {/* &nbsp; */}
+          {calc || "0"}
+        </div>
+        <div className="operators">
+          <button onClick={() => updatecalc("/")}>/</button>
+          <button onClick={() => updatecalc("*")}>*</button>
+          <button onClick={() => updatecalc("+")}>+</button>
+          <button onClick={() => updatecalc("-")}>-</button>
+
+          <button onClick={deleteCalc}>DEL</button>
+        </div>
+        <div className="digits">
+          {createDigits()}
+          <button onClick={() => updatecalc("0")}>0</button>
+          <button onClick={() => updatecalc(".")}>.</button>
+          <button onClick={declResult}>=</button>
+        </div>
+      </div>
     </div>
   );
 }
